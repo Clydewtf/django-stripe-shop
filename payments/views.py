644993faf436eaba9cf6d47fdf5a79b4ca7f0d20
Clydewtf <legitskull@mail.ru>
2025-12-22 21:19:36 +0700

    Revert "feat: add url to payments for /item/{id} page"
    This reverts commit 472d55f1173c8541eaab0cb6c96f075065620e0d.

--- payments/views.py
+++ payments/views.py
@@ -1,14 +1,3 @@
-from django.shortcuts import get_object_or_404, render
-from .models import Item
+from django.shortcuts import render
 
-
-def item_detail(request, item_id: int):
-    item = get_object_or_404(Item, id=item_id)
-
-    return render(
-        request,
-        "item_detail.html",
-        {
-            "item": item,
-        },
-    )
+# Create your views here.
