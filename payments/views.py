472d55f1173c8541eaab0cb6c96f075065620e0d
Clydewtf <legitskull@mail.ru>
2025-12-22 21:18:57 +0700

    feat: add templates dir and item detail page (/item/{id})

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Item
 
-# Create your views here.
+
+def item_detail(request, item_id: int):
+    item = get_object_or_404(Item, id=item_id)
+
+    return render(
+        request,
+        "item_detail.html",
+        {
+            "item": item,
+        },
+    )
