913d24fd54a643c4847477fe8fe1629ba932ff1a
Clydewtf <legitskull@mail.ru>
2025-12-23 00:35:30 +0700

    feat: add function that creates checkout session and returns it id

---
+++ payments/services/stripe_service.py
@@ -0,0 +1,29 @@
+import stripe
+from django.conf import settings
+from payments.models import Item
+
+
+def create_checkout_session(item: Item, success_url: str, cancel_url: str) -> str:
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    session = stripe.checkout.Session.create(
+        mode="payment",
+        payment_method_types=["card"],
+        line_items=[
+            {
+                "price_data": {
+                    "currency": item.currency,
+                    "unit_amount": item.price,
+                    "product_data": {
+                        "name": item.name,
+                        "description": item.description,
+                    },
+                },
+                "quantity": 1,
+            }
+        ],
+        success_url=success_url,
+        cancel_url=cancel_url,
+    )
+
+    return session.id
