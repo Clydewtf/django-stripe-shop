26535e65e6f4a50eda43626e67e0e7aa14a946e8
Clydewtf <legitskull@mail.ru>
2025-12-22 21:06:11 +0700

    feat: add item model w/ currency and admin panel

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.PositiveIntegerField(
+        help_text="Price in smallest currency unit (e.g. 100 cents for USD)"
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_EUR = "eur"
+
+    CURRENCY_CHOICES = [
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_EUR, "EUR"),
+    ]
+
+    currency = models.CharField(
+        max_length=3,
+        choices=CURRENCY_CHOICES,
+        default=CURRENCY_USD,
+    )
+
+    def __str__(self) -> str:
+        return self.name  # type: ignore
